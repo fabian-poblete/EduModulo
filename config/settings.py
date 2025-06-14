1affee5a03034bf0dc9ae44cc61c2b3cd197b11e
Fabian <fabian.poblete@cftsa.cl>
2025-06-13 22:42:44 -0400

    logging

--- config/settings.py
+++ config/settings.py
@@ -203,3 +203,19 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+
+# Logging para producción
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
