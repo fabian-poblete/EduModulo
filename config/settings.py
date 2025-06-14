f4e43920cb62f745892525ae0a1b9479a5d5739b
Fabian <fabian.poblete@cftsa.cl>
2025-06-13 22:32:35 -0400

    Debug False

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'clave-secreta-provisoria-para-build')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 
 
 ALLOWED_HOSTS = os.environ.get(
