01805c4c85657c1c5e54c4b4d9e60df06aa7dc5e
Fabian <fabian.poblete@cftsa.cl>
2025-07-10 15:23:43 -0400

    health_check

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
+
 urlpatterns = [
+    path("ping/", health_check),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # API URLs
     path('', include('public.urls')),
